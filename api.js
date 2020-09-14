a5bba71c4fda14022cba3a910a46321b32bf3b98
JUILLET Jasnel <40551326+jasneljuillet@users.noreply.github.com>
2020-09-14 10:15:07 -0700

    Update api.js

--- api.js
+++ api.js
@@ -8,27 +8,31 @@ cloudinary.config({
     api_secret: 'e_Y2MlfDz62e8vZq0Yjmhf5qr1U'
 });
 
-
 export default createLambda(
-    post(async ({ req, ok, error }) => {
-        var form = new multiparty.Form();
-        form.parse(req, (err, fields, files)=>{
-             const path = files.upload[0].path
-             const type = files.upload[0].headers['content-type']
-             const typesImage = ['image/png','image/jpg','image/jpeg','image/gif','image/bmp','image/tiff']
-            if(typesImage.includes(type)){
-                cloudinary.uploader.upload(path, (err, result)=>{
-                    if(err){
-                        console.log(err)
-                    }else{
-                        ok({success: true})
-                    }
-                })
-            }else{
-               ok('Choose image file')
+  post(async ({ req, ok, error }) => {
+    const form = new multiparty.Form();
+    const getForm = await form.parse(req, (err, fields, files) => {
+      const typesImage = [
+        "image/png",
+        "image/jpg",
+        "image/jpeg",
+        "image/gif",
+        "image/bmp",
+        "image/tiff",
+      ];
+      if (typesImage.includes(files.upload[0].headers["content-type"])) {
+        const uploader = cloudinary.v2.uploader.upload(
+          files.upload[0].path,
+          { folder: "niouz/images/", use_filename: true },
+          (err, result) => {
+            if (err) {
+              error(err);
+            } else {
+              ok({ success: true });
             }
-            
-        })
-         
-    })
-  );
+          }
+        );
+      }
+    });
+  })
+);
